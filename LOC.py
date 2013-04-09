8e1527a3448b747a62f203e77a88f66d0df4fcdc
Cameron Lakenen <cameron@crocodoc.com>
2013-04-09 12:02:01 -0700

    Initial commit

---
+++ LOC.py
@@ -0,0 +1,24 @@
+import re
+import sublime
+import sublime_plugin
+
+
+class LocCommand(sublime_plugin.EventListener):
+    def on_modified(self, view):
+        chars = view.size()
+        region = sublime.Region(0, chars)
+        contents = view.substr(region)
+
+        # total number of lines
+        loc = len(view.lines(region))
+
+        sloc = 0
+        # find all non-empty lines
+        pattern = re.compile(r"^\s*(\S+).*$", re.MULTILINE)
+        for m in pattern.finditer(contents):
+            syntax = view.scope_name(m.start(1))
+            # does the line start with a comment?
+            if syntax.find('comment') < 0:
+                sloc += 1
+
+        view.set_status("loc", "Lines: " + str(loc) + " (SLOC: " + str(sloc) + ")")
---
+++ README.md
@@ -0,0 +1,3 @@
+# SublimeLOC #
+
+SublimeLOC counts the lines and [source lines](http://en.wikipedia.org/wiki/Source_lines_of_code) of code in your files while you edit them.
